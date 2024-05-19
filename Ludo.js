72a65c6caec92ae26da4a184130c2d3e628260c2
Antonewton Quima <104485879+AntonewtonQ@users.noreply.github.com>
2024-05-19 19:35:02 +0100

    Add files via upload

---
+++ Ludo.js
@@ -0,0 +1,326 @@
+import { BASE_POSITIONS, HOME_ENTRANCE, HOME_POSITIONS, PLAYERS, SAFE_POSITIONS, START_POSITIONS, STATE, TURNING_POINTS } from './constants.js';
+import { UI } from './UI.js';
+
+export class Ludo {
+    currentPositions = {
+        P1: [],
+        P2: [],
+        P3: [],
+        P4: [],
+    }
+
+    _diceValue;
+    get diceValue() {
+        return this._diceValue;
+    }
+
+    set diceValue(value) {
+        this._diceValue = value;
+
+        UI.setDiceValue(value);
+    }
+
+    _turn;
+    get turn() {
+        return this._turn;
+    }
+    set turn(value) {
+        this._turn = value;
+        UI.setTurn(value);
+    }
+
+    _state;
+    get state() {
+        return this._state;
+    }
+    set state(value) {
+        this._state = value;
+
+        if(value === STATE.DICE_NOT_ROLLED) {
+            UI.enableDice();
+            UI.unhighlightPieces();
+        } else {
+            UI.disableDice();
+        }
+    }
+
+    _isKill;
+    get isKill(){
+        return this._isKill
+    }
+    set isKill(value){
+        this._isKill=value;
+    }
+
+    constructor() {
+        console.log('Hello World! Lets play Ludo!');
+
+        // this.diceValue = 4;
+        // this.turn = 0;
+        // this.state = STATE.DICE_ROLLED;
+        this.listenDiceClick();
+        this.listenResetClick();
+        this.listenPieceClick();
+    
+
+        //this.setPiecePosition('P1', 400, 0);
+
+        this.resetGame();
+        
+        // this.setPiecePosition('P2', 0, 1);
+        // this.diceValue = 6;
+        // console.log(this.getEligiblePieces('P1'))
+        
+    }
+
+    listenDiceClick() {
+        UI.listenDiceClick(this.onDiceClick.bind(this))
+    }
+
+    onDiceClick() {
+        console.log('dice clicked!');
+       this.diceValue = 1 + Math.floor(Math.random() * 6);
+       
+  
+        this.state = STATE.DICE_ROLLED;
+        
+        this.checkForEligiblePieces();
+    }
+
+
+    /*POSSO PEGAR ESSA LÓGICA PARA ADICIONAR MAIS 20 QUANDO MATAR*/
+
+
+// função usada para verificar se o joagdor actual tem peças elegiveis (peças que estejam no tabuleiro)
+// paa jogar, ou seja, verifica se o player tem peças que possam se mover
+    checkForEligiblePieces() {
+        const player = PLAYERS[this.turn];
+        
+        // peças elegíveis para o jogador atual
+        const eligiblePieces = this.getEligiblePieces(player);
+
+        if(eligiblePieces.length) {
+
+            // highlight the pieces
+            UI.highlightPieces(player, eligiblePieces);
+
+        } else {
+
+            this.incrementTurn();
+        }
+    }
+
+    incrementTurn() {
+        this.turn = (this.turn + 1) % 4;
+        this.state = STATE.DICE_NOT_ROLLED;
+    }
+
+
+    
+
+
+
+    getEligiblePieces(player) {
+        return [0, 1, 2, 3].filter(piece => {
+            const currentPosition = this.currentPositions[player][piece];
+
+            if (currentPosition === HOME_POSITIONS[player]) {
+                return false;
+            }
+
+            if (BASE_POSITIONS[player].includes(currentPosition) && this.diceValue !== 6) {
+                return false;
+            }
+
+            if (HOME_ENTRANCE[player].includes(currentPosition) && this.diceValue > HOME_POSITIONS[player] - currentPosition) {
+                return false;
+            }
+
+            return true;
+        });
+    }
+
+    
+
+    listenResetClick() {
+        UI.listenResetClick(this.resetGame.bind(this))
+    }
+
+    resetGame() {
+        console.log('reset game');
+        this.currentPositions = structuredClone(BASE_POSITIONS);
+
+        PLAYERS.forEach(player => {
+            [0, 1, 2, 3].forEach(piece => {
+                this.setPiecePosition(player, piece, this.currentPositions[player][piece])
+            })
+        });
+        //this.turn, que indica o índice do jogador na lista PLAYERS.
+
+        this.turn = 0;
+        this.state = STATE.DICE_NOT_ROLLED;
+    }
+
+    listenPieceClick() {
+        console.log("teste")
+        UI.listenPieceClick(this.onPieceClick.bind(this));
+    }
+
+    onPieceClick(event) {
+        const target = event.target;
+        console.log("teste")
+
+        if(!target.classList.contains('player-piece') || !target.classList.contains('highlight')) {
+            console.log("teste")
+            return;
+        }
+        console.log('piece clicked')
+
+        const player = target.getAttribute('player-id');
+        const piece = target.getAttribute('piece');
+
+        this.handlePieceClick(player, piece);
+    
+    }
+
+
+    handlePieceClick(player, piece) {
+        const currentPosition = this.currentPositions[player][piece];
+
+        if (BASE_POSITIONS[player].includes(currentPosition)) {
+            this.setPiecePosition(player, piece, START_POSITIONS[player]);
+            this.state = STATE.DICE_NOT_ROLLED;
+            return;
+        }
+
+        UI.unhighlightPieces();
+        this.isKill = this.checkForKill(player, piece);
+
+        if (currentPosition + this.diceValue === HOME_POSITIONS[player]) {
+            this.setPiecePosition(player, piece, HOME_POSITIONS[player]);
+            this.state = STATE.DICE_NOT_ROLLED;
+            this.giveBonusMove(player);
+            return;
+        }
+
+        if (this.isKill) {
+            this.movePiece(player, piece, 20);
+        } else {
+            this.movePiece(player, piece, this.diceValue);
+        }
+    }
+
+    giveBonusMove(player) {
+        const eligiblePieces = this.getEligiblePieces(player);
+
+        if (eligiblePieces.length) {
+            UI.highlightPieces(player, eligiblePieces);
+            this.state = STATE.BONUS_MOVE;
+        } else {
+            this.incrementTurn();
+        }
+    }
+
+    setPiecePosition(player, piece, newPosition) {
+        this.currentPositions[player][piece] = newPosition;
+        UI.setPiecePosition(player, piece, newPosition)
+    }
+
+// Responsavel por mover as peças
+    movePiece(player, piece, moveBy) {
+        // this.setPiecePosition(player, piece, this.currentPositions[player][piece] + moveBy)
+        const interval = setInterval(() => {
+            this.incrementPiecePosition(player, piece);
+            moveBy--;
+
+            if(moveBy === 0) {
+                clearInterval(interval);
+
+                // check if player won
+                if(this.hasPlayerWon(player)) {
+                    alert(`Player: ${player} has won!`);
+                    this.resetGame();
+                    return;
+                }
+
+                const isKill = this.checkForKill(player, piece);
+
+                if (isKill || this.diceValue === 6 ) {
+                    // Se o dado foi rolado 6, permitir que o jogador role novamente
+                    this.state = STATE.DICE_NOT_ROLLED;
+                    return;
+                }
+    
+                
+
+                this.incrementTurn();
+            }
+        }, 200);
+    }
+
+    checkForKill(player, piece) {
+        const currentPosition = this.currentPositions[player][piece];
+        const opponents = ['P1', 'P2', 'P3', 'P4'];
+        let kill = false;
+
+        opponents.forEach(opponent => {
+            if(opponent !== player){
+            [0, 1, 2, 3].forEach(piece => {
+            const opponentPosition = this.currentPositions[opponent][piece];
+
+            if(currentPosition === opponentPosition && !SAFE_POSITIONS.includes(currentPosition)) {
+                
+                this.setPiecePosition(opponent, piece, BASE_POSITIONS[opponent][piece]);
+                kill = true;
+            }
+        });
+        }
+
+    });
+
+        
+        return kill
+    }
+
+
+
+
+    hasPlayerWon(player) {
+        return [0, 1, 2, 3].every(piece => this.currentPositions[player][piece] === HOME_POSITIONS[player])
+    }
+
+    incrementPiecePosition(player, piece) {
+        this.setPiecePosition(player, piece, this.getIncrementedPosition(player, piece));
+    }
+    
+    getIncrementedPosition(player, piece) {
+        const currentPosition = this.currentPositions[player][piece];
+
+        if (currentPosition === TURNING_POINTS[player]) {
+            // Perguntar ao jogador se deseja girar o mundo
+            UI.askPlayerForTurnOption().then(response => {
+                if (response === 'turn') {
+                    this.setPiecePosition(player, piece, (currentPosition + 1) % 80);
+                } else {
+                    this.setPiecePosition(player, piece, HOME_ENTRANCE[player][0]);
+                }
+
+                if (this.hasPlayerWon(player)) {
+                    alert(`Player: ${player} has won!`);
+                    this.resetGame();
+                } else {
+                    this.state = STATE.DICE_NOT_ROLLED;
+                }
+            });
+
+            return currentPosition; // Temporariamente retorna a mesma posição até que a resposta seja processada
+        }
+
+        if (currentPosition === 79) {
+            return 0;
+        }
+
+        return currentPosition + 1;
+    }
+    
+}
